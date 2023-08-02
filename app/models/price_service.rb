554cea6f722d2367860467d04c04dd00bfb94fcb
khalidlotfi <khalidlotfi2005@gmail.com>
2023-08-02 20:19:43 +0100

    adding fetch stock prices method
    Signed-off-by: khalidlotfi <khalidlotfi2005@gmail.com>

--- app/models/price_service.rb
+++ app/models/price_service.rb
@@ -1,13 +1,11 @@
-class PriceServise
-
+class PriceService
   def self.fetch_prices(ticker)
-    stocks_found = client.search keywords: "MSFT"
-    stocks_found.output # Return the hash retrieved
+    stocks_found = client.search(keywords: ticker)
   end
 
   def self.client
     @client ||= Alphavantage::Client.new(
-      key: Rails.application.credentials.dig(:alphavantage ,:key)
+      key: Rails.application.credentials.dig(:alphavantage, :key)
     )
   end
 end

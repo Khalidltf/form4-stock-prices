02b69a8dd13bbec6a201d0be31d47809e25ebf2a
khalidlotfi <khalidlotfi2005@gmail.com>
2023-08-03 19:26:29 +0100

    fetch company prices job done
    Signed-off-by: khalidlotfi <khalidlotfi2005@gmail.com>

--- app/jobs/fetch_company_prices_job.rb
+++ app/jobs/fetch_company_prices_job.rb
@@ -4,7 +4,7 @@ class FetchCompanyPricesJob < ApplicationJob
   def perform(company_id)
     # Do something later
 
-    company = Company.find(company_id)
+    company = Comapny.find(company_id)
     company.prices = PriceService.fetch_prices(company.ticker)
     company.save!
   end
